38cd78f32e37fed6f8810039a763597bd91c4ff2
Bernardo Santos <bphsantos01@gmail.com>
2023-03-14 08:46:01 +0000

    update

--- python/face.py
+++ python/face.py
@@ -7,6 +7,9 @@ import cv2  # opencv-python
 import numpy as np
 from PIL import Image
 
+known_faces = []
+known_names = []
+
 
 def random_id():
     characters = string.ascii_letters + string.digits
@@ -14,63 +17,59 @@ def random_id():
     return person_id
 
 
-class FaceRecognition:
-    face_locations = []
-    face_encodings = []
-    face_names = []
-    known_face_encodings = []
-    known_face_names = []
+def encode_faces():
+    for image in os.listdir('faces'):
+        img = face_recognition.load_image_file(f"faces/{image}")
+        try:
+            face = face_recognition.face_encodings(img)[0]
+            known_faces.append(face)
+            known_names.append(image)
+        except IndexError:
+            print("No face found in the provided image")
+            print("Removing corrupt image from database")
+            os.remove("faces/" + image)
 
-    def __init__(self):
-        self.encode_faces()
 
-    def encode_faces(self):
-        for image in os.listdir('faces'):
-            face_image = face_recognition.load_image_file(f"faces/{image}")
-            face_encoding = face_recognition.face_encodings(face_image)[0]
+def run_recognition():
+    video_capture = cv2.VideoCapture(0)
+    ret, frame = video_capture.read()
+    name = None
 
-            self.known_face_encodings.append(face_encoding)
-            self.known_face_names.append(image)
-        print(self.known_face_names)
+    # Only process every other frame of video to save time
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    def run_recognition(self):
-        video_capture = cv2.VideoCapture(0)
-        ret, frame = video_capture.read()
-        name = None
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
 
-        # Only process every other frame of video to save time
+    # Find all the faces and face encodings in the current frame of video
+    locations = face_recognition.face_locations(rgb_small_frame)
+    faces = face_recognition.face_encodings(rgb_small_frame, locations)
 
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
-
-        # Find all the faces and face encodings in the current frame of video
-        self.face_locations = face_recognition.face_locations(rgb_small_frame)
-        self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
-
-        self.face_names = []
-        for face_encoding in self.face_encodings:
+    if len(known_faces) > 0:
+        for face_encoding in faces:
             # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+            matches = face_recognition.compare_faces(known_faces, face_encoding)
 
             # Calculate the shortest distance to face
-            face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+            face_distances = face_recognition.face_distance(known_faces, face_encoding)
 
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = self.known_face_names[best_match_index]
+            best_match = np.argmin(face_distances)
+            if matches[best_match]:
+                name = known_names[best_match]
                 break
 
-        if name is None:
-            face = np.array(small_frame)
-            image = Image.fromarray(face)
-            image.save("faces/" + random_id() + ".jpg")
-        else:
-            print(name)
+    if name is None:
+        print("Face not recognized")
+        print("Saving image to the database")
+        face = np.array(small_frame)
+        image = Image.fromarray(face)
+        image.save("faces/" + random_id() + ".jpg")
+    else:
+        print("Face recognized")
+        print("Hello " + name)
 
 
 if __name__ == '__main__':
-    fr = FaceRecognition()
-    fr.run_recognition()
+    encode_faces()
+    run_recognition()
