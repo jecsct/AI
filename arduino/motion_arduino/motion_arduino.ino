858e33cee5833c7331ead1d036b5d444b7b3a03e
jecsct <jetravassos2001@gmail.com>
2023-03-14 15:51:42 +0000

    Motion base code
    Base code structure for the 'motion_arduino.ino' file.

---
+++ arduino/motion_arduino/motion_arduino.ino
@@ -0,0 +1,56 @@
+import 
+
+// define pins
+#define  motionSensorPin  2;  
+#define ledPin 3;           
+
+int alertedRasPi = 0;
+
+void setup() 
+{
+  pinMode(motionSensorPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  if (detectsMotion() && alertedRasPi ) 
+  {
+    sendMessageToRasPi();
+  } 
+  
+  if ( hasMessageToRead() )
+  {
+    readRasPiMessage();
+  }
+}
+
+int detectsMotion() 
+{
+  return digitalRead(motionSensorPin);
+}
+
+void sendMessageToRasPi()
+{
+  Serial.println("Tenho aqui gente");
+
+  alertedRasPi = 1;
+}
+
+void readRasPiMessage()
+{
+  String data = Serial.readStringUntil('\n');
+  Serial.print("You sent me: ");
+  Serial.println(data);
+
+  alertedRasPi = 0;
+}
+
+void hasMessageToRead()
+{
+  return Serial.available() > 0;
+}
+
+
